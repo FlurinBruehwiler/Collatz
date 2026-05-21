5b5cdbc1510a6991d1c58e1df12a9802833916ee
Flurin Brühwiler <bruhwiler.flurin@gmail.com>
2026-05-21 22:16:34 +0200

    remove nested function

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -4,6 +4,8 @@ using System.Runtime.CompilerServices;
 
 //Time(() => ProgUnoptimized(10_000_000));
 Time(() => ProgOptimized(90_000_000));
+Time(() => ProgOptimized(90_000_000));
+Time(() => ProgOptimized(90_000_000));
 
 long ProgOptimized(int x)
 {
@@ -11,14 +13,13 @@ long ProgOptimized(int x)
 
     ushort[] numToLength = new ushort[dictCutoff];
     long[] numbers = new long[1024];
-    int numbersCount = 0;
 
     long maxI = 1;
     long maxLen = 0;
 
     for (int i = 1; i < x; i += 2)
     {
-        if (GetCachedLen(i, out var len))
+        if (GetCachedLen(i, out var len, dictCutoff, numToLength))
         {
             if (len > maxLen)
             {
@@ -28,11 +29,11 @@ long ProgOptimized(int x)
         }
         else
         {
-            var lenOffset = GetSeries(i);
+            var lenOffset = GetSeries(i, out var numbersCount, numbers, dictCutoff, numToLength);
             for (var index = 0; index < numbersCount; index++)
             {
                 var i1 = numbers[index];
-                StoreLenInCache(i1, (ushort)(lenOffset + (numbersCount - index)));
+                StoreLenInCache(i1, (ushort)(lenOffset + (numbersCount - index)), dictCutoff, numToLength);
             }
 
             var newLen = lenOffset + numbersCount;
@@ -46,61 +47,61 @@ long ProgOptimized(int x)
 
     Console.WriteLine($"x: {x}, found: {maxI} (length: {maxLen})");
     return maxI;
+}
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    void StoreLenInCache(long k, ushort v)
+[MethodImpl(MethodImplOptions.AggressiveInlining)]
+void StoreLenInCache(long k, ushort v, int dictCutoff, ushort[] numToLength)
+{
+    if (k < dictCutoff)
     {
-        if (k < dictCutoff)
-        {
-            numToLength[k] = v;
-        }
+        numToLength[k] = v;
     }
+}
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    bool GetCachedLen(long f, out ushort l)
+[MethodImpl(MethodImplOptions.AggressiveInlining)]
+bool GetCachedLen(long f, out ushort l, int dictCutoff, ushort[] numToLength)
+{
+    l = 0;
+
+    if (f < dictCutoff)
     {
-        l = 0;
-
-        if (f < dictCutoff)
+        var a = numToLength[(int)f];
+        if (a == 0)
         {
-            var a = numToLength[(int)f];
-            if (a == 0)
-            {
-                return false;
-            }
-
-            l = a;
-            return true;
+            return false;
         }
 
-        return false;
+        l = a;
+        return true;
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    ushort GetSeries(long num)
+    return false;
+}
+
+[MethodImpl(MethodImplOptions.AggressiveInlining)]
+ushort GetSeries(long num, out int numbersCount, long[] numbers, int dictCutoff, ushort[] numToLength)
+{
+    numbersCount = 0;
+    do
     {
-        numbersCount = 0;
-        do
+        if (GetCachedLen(num, out var l, dictCutoff, numToLength))
         {
-            if (GetCachedLen(num, out var l))
-            {
-                return l;
-            }
+            return l;
+        }
 
-            numbers[numbersCount++] = num;
+        numbers[numbersCount++] = num;
 
-            if (num % 2 == 0)
-            {
-                num = num / 2;
-            }
-            else
-            {
-                num = 3 * num + 1;
-            }
-        } while (num > 1);
+        if (num % 2 == 0)
+        {
+            num = num / 2;
+        }
+        else
+        {
+            num = 3 * num + 1;
+        }
+    } while (num > 1);
 
-        return 0;
-    }
+    return 0;
 }
 
 long ProgUnoptimized(long x)
